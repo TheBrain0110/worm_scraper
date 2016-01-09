59afc17036c21aa75f71734f544c58e98e0d6b9f
beriukay <beriukay@gmail.com>
2016-01-08 21:02:45 -0800

    uploading shell script and modified worm_scraper ruby script

---
+++ make_ebook.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+function scrape {
+	echo "scraping " $1
+	ruby serial_scrape.rb -s $1 > ${1}.html
+	ebook-convert ${1}.html ${1}.mobi --authors "John McCrae" --title "${1}" --max-toc-links 500
+	rm ${1}.html
+}
+
+while getopts ":ahtw" opt; do
+	case $opt in
+		w) scrape "worm"
+		   exit 
+		;;
+		t) scrape "twig"
+		   exit
+		;;
+		p) scrape "pact"
+		   exit
+		;;
+		a) scrape "worm"
+		   scrape "pact"
+		   scrape "twig"
+		   exit
+		;;
+
+		h) echo "options are: -a for all, -h for help, -p for pact, -t for twig, -w for worm" 
+		exit 1
+		;;
+	esac
+done
